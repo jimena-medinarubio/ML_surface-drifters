44c0f88d417f17b0fb920403567d49b6b04dcf54
jimena-medinarubio <jimena.medinarubio@gmail.com>
2025-03-20 14:47:53 +0100

    create linear regression function

--- ml_driven_drifter_data_analysis/modeling/linear_regression.py
+++ ml_driven_drifter_data_analysis/modeling/linear_regression.py
@@ -2,28 +2,8 @@
 
 import xarray as xr
 import numpy as np
-import pandas as pd
-from pathlib import Path
-from haversine import haversine, Unit
-import matplotlib.pyplot as plt
-from plots import plot_trajs
-from scipy.ndimage import gaussian_filter1d
-import importlib
-from importnb import Notebook
-from pathlib import Path
-import sys
-from scipy.special import erfc
-from sklearn.svm import SVR
-import seaborn as sns
-import pandas as pd
 from config import DATA_DIR
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.preprocessing import StandardScaler
-
-from modeling.train import BlockingSpatioTemporalSeriesSplit
-
 from features import create_feature_matrix
 #%%
 dt_features=xr.open_datatree(f'{DATA_DIR}/interim/interpolated_atm_ocean_datasets.nc')
@@ -33,10 +13,39 @@ def linear_regression(y, X):
     model = LinearRegression().fit(X, y)
     coeffs= model.coef_
     r2_total = model.score(X, y)
-
-    print('slope', coeffs)
-
     return coeffs, r2_total
+
+def select_variables(features, velocity_component='vx', dir='U', relative_wind=False):
+
+    if relative_wind==False:
+        X= features[velocity_component]-features[dir]-features[f'{dir}stokes']
+        y= features[f'{dir}10']
+    
+    else:
+        X= features[velocity_component]-features[dir]-features[f'{dir}stokes']
+        y= features[f'{dir}10']-features[dir]
+    
+    return X, y
+
+def calculate_uncertainties_fit(features_matrix, relative_wind=False):
+    drifter_list=np.unique(features_matrix['drifter_id'])
+
+    coeffsu=[]
+    coeffsv=[]
+    
+    for i, drifter in enumerate(drifter_list):
+        features_drifter=features_matrix[features_matrix['drifter_id']==list(drifter_list).pop(i)]
+        Xu, yu= select_variables(features_drifter, 'vx', 'U', relative_wind=relative_wind )
+        Xv, yv= select_variables(features_drifter, 'vy', 'V', relative_wind=relative_wind)
+        coeff_u, r2u= linear_regression(Xu, yu.values.reshape(-1, 1))
+        coeff_v, r2v= linear_regression(Xv, yv.values.reshape(-1, 1))
+        coeffsu.append(coeff_u)
+        coeffsv.append(coeff_v)
+
+    print('u:', np.mean(coeffsu), np.std(coeffsu))
+    print('v:', np.mean(coeffsv), np.std(coeffsv))
+
+    return coeffsu, coeffsv
 
 # %%
 features=create_feature_matrix(dt_features, ['U', 'V', 'U10', 'V10', 'Ustokes', 'Vstokes', 'Tp', 'vx', 'vy'], waves=False, fc=False)
@@ -44,24 +53,23 @@ features=create_feature_matrix(dt_features, ['U', 'V', 'U10', 'V10', 'Ustokes', 
 # %%
 
 #1st-order approximation
-
-Xu=features['vx']-features['U']-features['Ustokes']
-Xv=features['vy']-features['V']-features['Vstokes']
-
-yu=features['U10']
-yv=features['V10']
+Xu, yu= select_variables(features, 'vx', 'U', relative_wind=False )
+Xv, yv= select_variables(features, 'vy', 'V', relative_wind=False )
 
 coeff_u, r2u= linear_regression(Xu, yu.values.reshape(-1, 1))
 coeff_v, r2v= linear_regression(Xv, yv.values.reshape(-1, 1))
+
+coeffsu, coeffsv= calculate_uncertainties_fit(features, False)
 # %%
 #1st-order approximation: relative wind
 
-Xu=features['vx']-features['U']-features['Ustokes']
-Xv=features['vy']-features['V']-features['Vstokes']
-
-yu=features['U10']-features['U']
-yv=features['V10']-features['V']
+Xu, yu= select_variables(features, 'vx', 'U', relative_wind=True)
+Xv, yv= select_variables(features, 'vy', 'V', relative_wind=True)
 
 coeff_u, r2u= linear_regression(Xu, yu.values.reshape(-1, 1))
 coeff_v, r2v= linear_regression(Xv, yv.values.reshape(-1, 1))
+
+coeffsu, coeffsv= calculate_uncertainties_fit(features, True)
+# %%
+
 # %%
