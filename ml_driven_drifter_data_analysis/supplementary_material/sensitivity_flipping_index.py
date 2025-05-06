93c81f3bd35c6a7a256f539ef834ac84b79d3af7
jimena-medinarubio <jimena.medinarubio@gmail.com>
2025-05-06 14:28:24 +0200

    sensitivity analysis of drifter data

--- ml_driven_drifter_data_analysis/supplementary_material/sensitivity_flipping_index.py
+++ ml_driven_drifter_data_analysis/supplementary_material/sensitivity_flipping_index.py
@@ -7,10 +7,11 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes, mark_inset
 import matplotlib.patches as patches
 import seaborn as sns
-
+import sys
+sys.path.append("..")
 from processing_drifter_data import flipping_index_continuous, select_frequency_interval
 #%%
-PROJ_ROOT = Path(__file__).resolve().parents[1]
+PROJ_ROOT = Path(__file__).resolve().parents[2]
 DATA_DIR = PROJ_ROOT / "data"
 
 datatree=xr.open_datatree(f'{DATA_DIR}/interim/processed_drifter_data.nc')
@@ -31,7 +32,7 @@ example_drifter='0510'
 
 def sensitivity_analysis_fi_plot(example_drifter, datatree, dict_flipping_index, delta_times, output_path=None, output_format='svg', name_fig='sensitivity_analysis_fi'):
     # Create the main plot
-    plt.figure(figsize=(12, 7))
+    plt.figure(figsize=(12, 6))
 
     # Color map for different lines
     color_map = [ '#A6CEE3', '#FF7F00',  'teal', '#E41A1C',  '#F781BF',  '#4DAF4A',  'royalblue', ]
@@ -41,7 +42,7 @@ def sensitivity_analysis_fi_plot(example_drifter, datatree, dict_flipping_index,
         time= (datatree[example_drifter]['time'] - datatree[example_drifter]['time'][0]).astype('timedelta64[s]').astype(int)
         plt.plot(time.values/3600/24, dict_flipping_index[str(t)][example_drifter], label=f'{t} h', linewidth=2, color=color_map[idx], alpha=0.8)
 
-    plt.legend(fontsize=14, loc='upper left')
+    plt.legend(fontsize=14, loc='best')
     # Labels and formatting
     plt.xlabel('Time since release [days]', fontsize=14)
     plt.ylabel('Flipping Index', fontsize=14)
@@ -50,7 +51,7 @@ def sensitivity_analysis_fi_plot(example_drifter, datatree, dict_flipping_index,
 
     # Inset zoom: choose a region of interest (e.g., a peak area)
     # Adjust the zoom window to the region where the peaks are (example range)
-    zoom_xlim = [5.5, 7.75]  # Define the x-limits for the zoom window (adjust these values based on your data)
+    zoom_xlim = [5, 6]  # Define the x-limits for the zoom window (adjust these values based on your data)
 
     zoom_ylim = [0.13, 0.28]  # Define the y-limits for the zoom window (adjust these values based on your data)
 
@@ -58,7 +59,7 @@ def sensitivity_analysis_fi_plot(example_drifter, datatree, dict_flipping_index,
                             linewidth=1, edgecolor='black', facecolor='none', linestyle='--', label='Zoom-in Area')
     plt.gca().add_patch(rect)
     # Create inset axes for the zoomed-in area
-    axins = inset_axes(plt.gca(), width="30%", height="30%", loc='upper right')  # Position the zoom window
+    axins = inset_axes(plt.gca(), width="30%", height="40%", loc='upper center')  # Position the zoom window
 
     # Plot the zoomed-in region
     for idx, t in enumerate(delta_times):
@@ -70,12 +71,14 @@ def sensitivity_analysis_fi_plot(example_drifter, datatree, dict_flipping_index,
     axins.set_ylim(zoom_ylim)
 
     axins.tick_params(axis='both', labelsize=12) 
+    axins.set_xlabel('Time [days]', fontsize=12)
+    axins.set_ylabel('FI', fontsize=12)
 
     # Draw a rectangle showing the zoom area on the main plot
     mark_inset(plt.gca(), axins, loc1=2, loc2=4, fc="none", ec="0.5")
 
     if output_path is None:
-            PROJ_ROOT = Path(__file__).resolve().parents[1]
+            PROJ_ROOT = Path(__file__).resolve().parents[2]
             FIGURES_DIR = PROJ_ROOT / "reports" / "figures"
             output_path = FIGURES_DIR / f'{name_fig}.{output_format}'
 
