84d9755293b1e824d30764c935839af6845930c4
Jimena Medina <jimena.medinarubio@gmail.com>
2025-06-13 12:54:36 +0200

    cleaning pre-processing file

--- ml_driven_drifter_data_analysis/preprocessing_drifter_data.py
+++ ml_driven_drifter_data_analysis/preprocessing_drifter_data.py
@@ -7,14 +7,28 @@ from pathlib import Path
 from haversine import haversine, Unit
 from plots import plot_trajs
 #%%
+#specify directories
 PROJ_ROOT = Path(__file__).resolve().parents[2]
 DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
+REFERENCES_DIR = PROJ_ROOT / "references"
 
-ds_og=xr.open_dataset(f'{RAW_DATA_DIR}/waddendrifters2024.nc')
-df_drifters = pd.read_csv(f'{PROJ_ROOT}/references/drifters_info.csv',  delimiter=';', dtype={'ID': str})
+
+#define drifter file
+drifter_file='waddendrifters2024.nc'
+# define drifter ID file
+drifter_id_file='drifters_info.csv'
+# velocity data
+velocity_file='external/velocity_land_mask.nc'
+
+#open datasets
+ds_og=xr.open_dataset(f'{RAW_DATA_DIR}/{drifter_file}')
+df_drifters = pd.read_csv(f'{REFERENCES_DIR}/{drifter_id_file}',  delimiter=';', dtype={'ID': str})
 drifters_id_dict = dict(zip(df_drifters['Drifter'], df_drifters['ID']))
+#land mask
+velocity_land_mask = xr.open_dataset(f'{DATA_DIR}/{velocity_file}')
 
+#create xarray DataTree from the original dataset
 ds={str(df_drifters['ID'].values[i]): ds_og.sel(trajectory=df_drifters['Drifter'].values[i]).swap_dims({"obs": "time"}) for i in range(len(ds_og.trajectory.values))}
 dt_og = xr.DataTree.from_dict(ds)
 
@@ -23,7 +37,13 @@ dt_og = xr.DataTree.from_dict(ds)
 def remove_after_land(dt, velocity_ds, buffer_time=500, initial_time=True):
     """
     Remove all time coordinates from all datasets after the first land encounter.
+
+    dt: xarray.DataTree with drifter coordinates
+    velocity_ds: xarray.Dataset with velocity data to create land mask
+    buffer_time: int, time in minutes to allow before first land encounter to filter out intertidal zone
+    initial_time: bool, if True, allow initial buffer time of 24h for analysis
     """
+    #create land mask from velocity dataset where velocity is NaN
     land_mask = np.where(np.isnan(velocity_ds['uo'][0]), 0, 1)
     mask = RegularGridInterpolator((velocity_ds['latitude'].values, velocity_ds['longitude'].values), land_mask, method='nearest')
     
@@ -67,6 +87,9 @@ def remove_after_land(dt, velocity_ds, buffer_time=500, initial_time=True):
 def filter_time(dt, threshold=2.5):
     """
     Filter out time steps where the time difference is larger than the threshold.
+
+    dt: xarray.DataTree with drifter coordinates
+    threshold: float, maximum allowed time difference in seconds between consecutive observations.
     """
     dtc = dt.copy()  # Copy the tree to avoid modifying the original
     for node in dt.leaves:  # Iterate over all leaf nodes
@@ -87,12 +110,8 @@ def eliminate_signal_errors(dt, threshold=3):
     Remove signal errors by filtering out observations where the speed 
     between consecutive points exceeds a given threshold.
 
-    Parameters:
-    dt (xarray.DataTree): A datatree with datasets containing dimensions ('obs', 'trajectory') and variables 'lon', 'lat', 'time'.
-    threshold (float): Maximum allowed speed (in consistent units with distance/time).
-
-    Returns:
-    xarray.DataTree: A new datatree with erroneous observations removed.
+    dt: xarray.DataTree with drifter coordinates.
+    threshold: float, maximum allowed instantaneous speed (in consistent units with distance/time).
     """
 
     datasets = {}
@@ -116,15 +135,12 @@ def eliminate_signal_errors(dt, threshold=3):
     return xr.DataTree.from_dict(datasets)
 
 #%%
-#land mask
-velocity_land_mask = xr.open_dataset(f'{PROJ_ROOT}/data/external/velocity_land_mask.nc')
 
 #apply funcitons to data
 dt = remove_after_land(dt_og, velocity_land_mask, initial_time=False)
-#%%
 dt = filter_time(dt)
 dt = eliminate_signal_errors(dt)
 #%%
-# Save or further process the DataTree (dt) as needed
+# save pre-processed DataTree to a NetCDF file
 dt.to_netcdf(f'{PROJ_ROOT}/data/interim/preprocessed_drifter_data.nc')  
 # %%
